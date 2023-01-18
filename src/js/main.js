4be6a16bd1dc5a44711387e4a07606612d0b4cf5
Dmitry <free-doom46@mail.ru>
2023-01-18 22:56:46 +0300

    adds burger js

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,13 @@
-let a = "Привет"
+
+let navHeader = document.querySelector('.nav--header');
+let navClosed = document.querySelector('.nav--opened');
+let burger = document.querySelector('.burger');
 
 
-console.log(a);
+burger.addEventListener('click', () => {
+  navHeader.classList.toggle('nav--opened');
+  burger.classList.toggle('burger--opened');
+})
+
+// navClosed.classList.toggle
+// console.log(navClosed);
