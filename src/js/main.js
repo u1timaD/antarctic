b7b0be9c641a17f4711fec52a8024a6a01cc0e58
Dmitry <free-doom46@mail.ru>
2023-01-19 13:06:23 +0300

    creates product cards/ fixes burger js

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,26 @@
 let navHeader = document.querySelector('.nav--header');
 let burger = document.querySelector('.burger');
 let logoHeader = document.querySelector('.logo--header');
+let body = document.querySelector('body');
+let burgerLink = document.querySelectorAll('.burger__link');
+
 
 burger.addEventListener('click', () => {
   navHeader.classList.toggle('nav--opened');
   burger.classList.toggle('burger--opened');
   logoHeader.classList.toggle('logo--burger');
+  body.classList.toggle('scroll-lock');
+  body.classList.toggle('shadow');
+})
+
+
+burgerLink.forEach(item => {
+item.addEventListener('click', () => {
+  navHeader.classList.toggle('nav--opened');
+  burger.classList.toggle('burger--opened');
+  logoHeader.classList.toggle('logo--burger');
+  body.classList.toggle('scroll-lock');
+  body.classList.toggle('shadow');
+});
 })
 
